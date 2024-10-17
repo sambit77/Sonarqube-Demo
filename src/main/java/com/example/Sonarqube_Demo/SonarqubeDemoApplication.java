d172c33b39eb0b78175ff0974f9511d16e2f0b9f
Sambit Pradhan <sambitgulu@gmail.com>
2024-10-17 19:24:23 +0530

    base project

---
+++ src/main/java/com/example/Sonarqube_Demo/SonarqubeDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.Sonarqube_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeDemoApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/example/Sonarqube_Demo/SonarqubeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sonarqube_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
